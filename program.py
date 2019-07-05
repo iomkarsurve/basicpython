bec455583b774c4ea05642c9aa13785bd685b0a5
iomkarsurve <47293371+iomkarsurve@users.noreply.github.com>
2019-07-05 20:36:15 +0530

    Add files via upload
    Code On Topic :
    
    printing in python
    storing value in variable
    addition of numbers
    Taking input from user

---
+++ program.py
@@ -0,0 +1,20 @@
+print("Alright") #for printing
+
+# variables to store values
+
+#number string list tuple dictionary
+
+x = 10 #integer var
+y = "HI" #string var
+
+var=101
+word="this is a string"
+
+print(var)
+print(word)
+
+a=int(input("Enter value of first number")) #to take input from user
+b=int(input("Enter value of 2nd number"))
+print("Sum is ",a+b)
+
+
