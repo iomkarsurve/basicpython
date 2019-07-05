d4f99ed3ad86a163ef13ecc2e16bec8876a7ab4b
iomkarsurve <47293371+iomkarsurve@users.noreply.github.com>
2019-07-05 22:38:35 +0530

    Add files via upload

---
+++ conditional statements/conditional statements.py
@@ -0,0 +1,20 @@
+#if statement
+x= int(input("enter value of x "))
+if(x>5): #if true print x
+    print(x)
+
+
+#elif
+
+y = int(input("enter value of y "))
+if(y==1):
+    print("A")
+elif(y==2):
+    print("B")
+elif(y==3):
+    print("C")
+elif(y==4):
+    print("D")
+else:
+    print("please enter number between 1 and 4") #else statement
+
