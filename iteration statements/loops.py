90732d447ffbf480bbb82b1dfd9834b2bddc548b
iomkarsurve <47293371+iomkarsurve@users.noreply.github.com>
2019-07-06 18:49:26 +0530

    Add files via upload

---
+++ iteration statements/loops.py
@@ -0,0 +1,13 @@
+#printing till 10
+for i in range(0,10):
+    print(i)
+
+# while loops
+#print from entered number till more than 5 that is 6 (>5)
+i= int(input("Enter the number"))
+while(i>5):
+    print(i)
+    i=i-1    #by removing this line loop enters infinite loop need to be terminated
+
+
+
